6c2acd7c2089cbf87cb515e3d0974dad7377cec3
Arsenii Krasnyh <mr.kranyh@mail.ru>
2025-05-15 18:31:15 +0500

    first lesson

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let title = 1;
+let screens = 2;
+let screenprice = 3;
+let rollback = 4;
+let fullprice = 5;
+let adaptive = 6;
+
+
+alert ("Hello, dear МамоЮзер")
+console.log("Ты точно МамоЮзер?")
