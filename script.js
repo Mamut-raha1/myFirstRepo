a9fbfbabcbd97386922bf21e4d9ddcbbd733142b
Arsenii Krasnyh <mr.kranyh@mail.ru>
2025-05-19 14:59:45 +0500

    Задание третьего урока

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 "use strict"
 
-let title = "Prject name";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 99;
+let title = prompt ('Как называется ваш проект?', 'Введите название проекта');
+let screens = prompt ('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt ('Сколько будет стоить данная работа?', '12000');
 let rollback = 52;
-let fullPrice = 99554433;
-let adaptive = true;
+let adaptive = confirm ('Нужен ли адаптив на сайте?');
 
+let service1 = prompt ('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = +prompt ('Сколько это будет стоить?')
+let service2 = prompt ('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = +prompt ('Сколько это будет стоить?')
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice-rollback)
+console.log(servicePercentPrice)
+
+switch(true) {
+    case fullPrice>=30000:
+        console.log('Даем скидку в 10%');
+        break
+    case 15000<=fullPrice ?? fullPrice <30000:
+        console.log('Даем скидку в 5%');
+        break
+    case 0<=fullPrice ?? fullPrice <15000:
+        console.log('Скидка не предусмотрена');
+        break
+    default:
+        console.log('Что-то пошло не так');
+}
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
@@ -18,4 +40,4 @@ console.log(`Стоимость разработки сайта ${fullPrice} д�
 
 console.log(screens.toLowerCase().split(", "));
 
-console.log(fullPrice*(rollback/100))
+console.log(fullPrice*(rollback/100));
